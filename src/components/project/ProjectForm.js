f3190d5fcbd1aa7ca74b08ea6d5064d778bd9242
Hector Salgueiros <99827871+hectorsalg@users.noreply.github.com>
2022-11-15 18:07:35 -0300

    create the form of page newproject

---
+++ src/components/project/ProjectForm.js
@@ -0,0 +1,26 @@
+import Input from '../form/Input';
+import Select from '../form/Select';
+import SubmitButton from '../form/SubmitButton';
+
+import styles from './ProjectForm.module.css';
+
+export default function ProjectForm({ btnText }) {
+    return (
+        <form className={styles.form}>
+            <Input 
+                type='text' 
+                text='Nome do projeto' 
+                name='name' 
+                placeholder='Insira o nome do projeto'
+            />
+            <Input 
+                type='number' 
+                text='Orçamento do projeto' 
+                name='budget' 
+                placeholder='Insira o orçamento total'
+            />
+            <Select name='categoryId' text='Selecione a Categoria' />
+            <SubmitButton text={btnText}/>
+        </form>
+    );
+}
