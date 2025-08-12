ade373a30ee59776f8c682c284fa8428be14f4fa
Hectorsalg <hectorsalg@ufpi.edu.br>
2025-08-12 13:15:47 -0300

    feat: links das redes sociais

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,4 +1,4 @@
-import { FaInstagram, FaLinkedin, FaGithub } from "react-icons/fa";
+import {FaLinkedin, FaGithub } from "react-icons/fa";
 
 import styles from "./Footer.module.css";
 
@@ -9,13 +9,10 @@ export default function Footer() {
     <footer className={styles.footer}>
       <ul className={styles.socialList}>
         <li>
-          <FaInstagram />
+          <a href="https://www.linkedin.com/in/hectorsalg/" target="_blank"><FaLinkedin /></a>
         </li>
         <li>
-          <FaLinkedin />
-        </li>
-        <li>
-          <FaGithub />
+          <a href="https://github.com/hectorsalg" target="_blank"><FaGithub /></a>
         </li>
       </ul>
       <p className={styles.copyright}>
